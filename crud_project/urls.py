ba9f052798e273a3760407e12eab13d06f1f772b
Radzivon-Karachun <70255645+Radzivon-Karachun@users.noreply.github.com>
2022-10-23 18:12:08 +0200

    add new proj

--- crud_project/urls.py
+++ crud_project/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from crud_project import views
 
 
 urlpatterns = [
--- django_CRUD_Project/settings.py
+++ django_CRUD_Project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crud_project',
+    'auth_system',
 ]
 
 MIDDLEWARE = [
