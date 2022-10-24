fd8d5aecd5357a1877980ec751a5dd39593e174a
Radzivon-Karachun <70255645+Radzivon-Karachun@users.noreply.github.com>
2022-10-24 19:39:01 +0200

    Add template Signup, Login, Logout

--- auth_system/views.py
+++ auth_system/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def signup_page(request):
+    return render(request, 'auth_system/signup.html')
+
+
+def login_page(request):
+    return render(request, 'auth_system/login.html')
+
+
+def logout_page(request):
+    return render(request, 'auth_system/logout.html')
--- crud_project/urls.py
+++ crud_project/urls.py
@@ -1,16 +1,24 @@
 from django.urls import path
-from crud_project import views
+from crud_project.views import home, CreateBlog_page, DetailBlog_page, UpdateBlog_page, DeleteBlog_page
+from auth_system.views import signup_page, login_page, logout_page
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', home, name='home'),
 
-    path('create/', views.CreateBlog_page.as_view(),
+    path('create/', CreateBlog_page.as_view(),
          name='create_blog_page'),
-    path('<int:pk>/', views.DetailBlog_page.as_view(),
+    path('<int:pk>/', DetailBlog_page.as_view(),
          name='detail_blog_page'),
-    path('<int:pk>/update/', views.UpdateBlog_page.as_view(),
+    path('<int:pk>/update/', UpdateBlog_page.as_view(),
          name='update_blog_page'),
-    path('<int:pk>/delete/', views.DeleteBlog_page.as_view(),
+    path('<int:pk>/delete/', DeleteBlog_page.as_view(),
          name='delete_blog_page'),
+
+    path('signup', signup_page,
+         name='signup_page'),
+    path('login', login_page,
+         name='login_page'),
+    path('logout', logout_page,
+         name='logout_page'),
 ]
